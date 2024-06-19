ea3e9a535cb3ea2fc1d7dfcb436c8bb3fb1cee30
Gabodor <gabriel.fumagalli@studenti.unitn.it>
2024-06-19 16:04:50 +0200

    Loading current work

---
+++ test1/client.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import sys
+from project_interfaces.srv import Pose
+
+import time
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+
+from roboticstoolbox import quintic, mtraj
+
+STEP_MAX = 10
+FREQUENCY = 4
+TIME_STEP = 1/FREQUENCY
+TIME_MAX_SEC = STEP_MAX/FREQUENCY
+
+class Ur3_controller(Node):
+    def __init__(self):
+        super().__init__('ur3_controller')
+        self.cli = self.create_client(Pose, 'test1')
+        self.publisher_ = self.create_publisher(PoseStamped, 'topic', 10)               # target_frame(Coppeliasim) / topic(Listener)
+
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = Pose.Request()
+
+        self.trajectory_planning()
+        self.move_first()
+        self.move_second()
+
+    def send_request(self, r):
+        self.req.r = r
+        future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
+    
+    def trajectory_planning(self):                                                          #da rivedere
+        # Deve chiedere configurazione attuale end-effector e finale
+        response = self.send_request(0)
+        self.get_logger().info('Requesting first quaternion to compute planning')
+
+        q_start=(-0.13088, 0.29877, 0.30323, 0.99999, 0.00039, 0.00039, 0.00039)
+
+        #q_end=(0.37158, 0.15214, 0.32538, 0.99999, 0.00039, 0.00039, 0.00039)
+        q_end=(0.37158, 0.15214, 0.32538, response.x, response.y, response.z, response.w)
+        self.trajectory = mtraj(quintic, q_start, q_end, STEP_MAX)
+    
+    def publish_pose(self, position, orientation):
+        msg = PoseStamped()
+
+        msg.header.frame_id = "base_link"
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.pose.position.x = position[0]
+        msg.pose.position.y = position[1]
+        msg.pose.position.z = position[2]
+        msg.pose.orientation.x = orientation[0]                       #controlla ordine x y z w
+        msg.pose.orientation.y = orientation[1]
+        msg.pose.orientation.z = orientation[2]
+        msg.pose.orientation.w = orientation[3]
+    
+        self.publisher_.publish(msg)
+        #self.get_logger().info('Publishing pose: [ position: (x:%f, y:%f, z:%f), orientation (x:%f, y:%f, z:%f, w:%f)]'
+        #                        % (position[0], position[1], position[2], orientation[0], orientation[1], orientation[2], orientation[3]))
+        self.get_logger().info('Publishing pose')
+                               
+    def move_first(self):
+        self.time = 0
+        while self.time < STEP_MAX :
+            q = self.trajectory.q[self.time]
+            position = (q[0], q[1], q[2])
+            orientation = (q[3], q[4], q[5], q[6])
+            self.publish_pose(position, orientation)
+            time.sleep(TIME_STEP)      
+            self.time = self.time+1
+        print("\nMove first complete\n")
+    
+    def move_second(self):
+        while True:
+            response = self.send_request(0)
+            position = (0.37158, 0.15214, 0.32538)
+            orientation = (response.x, response.y, response.z, response.w)
+
+            self.get_logger().info('Requesting quaternion')
+            self.publish_pose(position, orientation)
+
+            time.sleep(TIME_STEP)        
+        print("Move second complete")
+
+
+
+def main():
+    rclpy.init()
+
+    ur3_controller = Ur3_controller()
+    print("arrivato?")
+    rclpy.spin(ur3_controller)
+
+    ur3_controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test1/listener.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import PoseStamped
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            PoseStamped,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('\n Pose received: [ position: (x:%f, y:%f, z:%f), orientation (x:%f, y:%f, z:%f, w:%f)]\n'
+                                % (msg.pose.position.x,
+                                    msg.pose.position.y,
+                                    msg.pose.position.z,
+                                    msg.pose.orientation.x,
+                                    msg.pose.orientation.y,
+                                    msg.pose.orientation.z,
+                                    msg.pose.orientation.w))
+                                    
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
