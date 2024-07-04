77586b79909af03085f2e1dcbc00fab3d7dd7da6
Gabodor <gabriel.fumagalli@studenti.unitn.it>
2024-07-04 09:40:05 +0200

    eye movement visualization

--- test1/client.py
+++ test1/client.py
@@ -12,6 +12,7 @@ from tf2_ros.transform_listener import TransformListener
 from tf2_ros.transform_broadcaster import TransformBroadcaster
 from tf2_ros.buffer import Buffer
 from tf2_ros import TransformException
+from visualization_msgs.msg import  Marker
 
 from roboticstoolbox import quintic, mtraj
 import numpy as np
@@ -39,7 +40,9 @@ class Ur3_controller(Node):
         # Create publisher to publish planning
         self.publisher = self.create_publisher(PoseStamped, 'target_frame', 10)
 
-        self.broadcast_timer_callback()
+        # Create publisher to publish eye movement in the simulation 
+        self.marker_publisher = self.create_publisher(Marker, 'visualization_marker', 10)
+        self.tf_broadcaster = TransformBroadcaster(self)
         
         # Getting initial pose
         self.get_initial_pose()
@@ -47,24 +50,6 @@ class Ur3_controller(Node):
         self.trajectory_planning()
         self.move_first()
         self.move_second()
-
-    def broadcast_timer_callback(self):
-        tf_broadcaster = TransformBroadcaster(self)
-        t = TransformStamped()
-
-        t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = 'wrist_3_link'
-        t.child_frame_id = 'end_effector_extension'
-        t.transform.translation.x = 0.0
-        t.transform.translation.y = 0.0
-        t.transform.translation.z = 0.1
-        t.transform.rotation.x = 0.0
-        t.transform.rotation.y = 0.0
-        t.transform.rotation.z = 0.0
-        t.transform.rotation.w = 1.0
-
-        print("frame published")
-        tf_broadcaster.sendTransform(t)
 
     def get_initial_pose(self):
         self.tf_buffer = Buffer()
@@ -143,6 +128,8 @@ class Ur3_controller(Node):
         q_eye = np.array([response.w, response.x, response.y, response.z])
         q = self.quaternion_multiply(q_trans, q_eye)
 
+        self.environment_building(q)
+
         return q
     
     def publish_pose(self, position, orientation):
@@ -171,6 +158,98 @@ class Ur3_controller(Node):
                         x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
                         -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
                         x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
+    
+    def environment_building(self, rotation):
+        print("Vai su RVIZ:\n - ADD /rviz_default_plugin/Marker\n - change topic name to 'visualization_marker'")
+        
+        t = TransformStamped()
+
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'base_link'
+        t.child_frame_id = 'prova_frame'
+        t.transform.translation.x = -0.15
+        t.transform.translation.y = 0.65
+        t.transform.translation.z = 0.32538
+        t.transform.rotation.w = rotation[0]
+        t.transform.rotation.x = rotation[1]
+        t.transform.rotation.y = rotation[2]
+        t.transform.rotation.z = rotation[3]
+
+        self.tf_broadcaster.sendTransform(t)
+
+        marker = Marker()
+
+        marker.header.frame_id = "prova_frame"
+        marker.header.stamp = self.get_clock().now().to_msg()
+
+        marker.ns = "my_namespace"
+        marker.id = 0
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.a = 1.0
+        marker.color.r = 255.0 / 255.0
+        marker.color.g = 255.0 / 255.0
+        marker.color.b = 255.0 / 255.0
+
+        self.marker_publisher.publish(marker)
+
+        marker.header.frame_id = "prova_frame"
+        marker.header.stamp = self.get_clock().now().to_msg()
+
+        marker.ns = "my_namespace"
+        marker.id = 1
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = -0.04
+        marker.pose.orientation.w = 1.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.scale.x = 0.05
+        marker.scale.y = 0.05
+        marker.scale.z = 0.02
+        marker.color.a = 1.0
+        marker.color.r = 0.0 / 255.0
+        marker.color.g = 150.0 / 255.0
+        marker.color.b = 0.0 / 255.0
+
+        self.marker_publisher.publish(marker)
+
+        marker.header.frame_id = "prova_frame"
+        marker.header.stamp = self.get_clock().now().to_msg()
+
+        marker.ns = "my_namespace"
+        marker.id = 2
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = -0.05
+        marker.pose.orientation.w = 1.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.scale.x = 0.02
+        marker.scale.y = 0.02
+        marker.scale.z = 0.005
+        marker.color.a = 1.0
+        marker.color.r = 0.0 / 255.0
+        marker.color.g = 0.0 / 255.0
+        marker.color.b = 0.0 / 255.0
+
+        self.marker_publisher.publish(marker)
 
 
 def main():
